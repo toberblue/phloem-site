6f5bbf8264a4a35ae11c13a97342bbb9a4a3f8b6
Peter Matthews <peter@henley.net.nz>
2026-08-07 13:47:11 +1200

    workbench build for the cost trial

--- write/assets/__vite-browser-external-BFhQ10s6.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./sdk-CvKFzrcs.js";var t=e(((e,t)=>{t.exports={}}));export default t();
---
+++ write/assets/__vite-browser-external-CpZEHkwB.js
@@ -0,0 +1,1 @@
+import{t as e}from"./sdk-CE879mIj.js";var t=e(((e,t)=>{t.exports={}}));export default t();
